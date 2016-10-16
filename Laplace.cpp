b2634eb9f190b2ca49e494e221ed918f1cb805d1
Norifusa Tachiyanagi <wonderfulboyx@gmail.com>
2016-10-16 23:40:45 +0900

    first commit

---
+++ Laplace.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+
+
+typedef Eigen::SparseMatrix<double> sp_mat;
+typedef Eigen::Triplet<double> T;
+
+void buildProblem(std::vector<T> &coffs, Eigen::VectorXd &b, int n);
+
+int main(int argc, char **arfv)
+{
+    int n = 300;
+    int m = n * n;
+
+    std::vector<T> cofficients;
+    Eigen::VectorXd b(m);
+}
+
+void 
